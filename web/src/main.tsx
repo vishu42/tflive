6060b851d36b4eed518a11aa98b7aa927dd259c3
vishu42 <tewatiavishal10@gmail.com>
2026-07-19 13:02:52 +0530

    feat(web): provide QueryClient at the app root

--- web/src/main.tsx
+++ web/src/main.tsx
@@ -1,11 +1,17 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { QueryClientProvider } from "@tanstack/react-query";
 import { RouterProvider } from "react-router-dom";
+import { createQueryClient } from "./app/queryClient";
 import { router } from "./app/router";
 import "./styles.css";
 
+const queryClient = createQueryClient();
+
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <RouterProvider router={router} />
+    <QueryClientProvider client={queryClient}>
+      <RouterProvider router={router} />
+    </QueryClientProvider>
   </React.StrictMode>
 );
